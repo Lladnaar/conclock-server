23149773786b7cd292c63449e1fc3b1bbb3bd2ef
Lladnaar <mark.randall@email.com>
2025-08-10 20:20:51 +1200

    Added static file handler.

--- index.ts
+++ index.ts
@@ -3,16 +3,18 @@ import { router as timeRouter} from './time.ts';
 
 const app = express();
 const port = 9000;
+const appfiles = 'app';
 
 // use the json middleware
 app.use(express.json());
 
 app.get('/', (req: express.Request, res: express.Response) => {
   res.send({
-    'time': {'url': `${req.protocol}://${req.get('host')}/time`}
+    'time': {'url': `${req.protocol}://${req.host}/time`}
   });
 });
 
+app.use('/app', express.static(appfiles));
 app.use('/time', timeRouter);
 
 app.all('/{*any}', (req: express.Request, res: express.Response, next: express.NextFunction) => {
