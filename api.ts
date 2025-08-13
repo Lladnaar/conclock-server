141b5bee8b4d86effc2dd8ba965795aeabf1e4ed
Lladnaar <mark.randall@email.com>
2025-08-13 15:41:07 +1200

    Moved and cleaned up URIs

---
+++ api.ts
@@ -0,0 +1,15 @@
+// time resource definition
+
+import express from 'express';
+
+const router = express.Router();
+
+// Resource and verb definitions 
+
+router.get('/', (req: express.Request, res: express.Response) => {
+  res.send({
+    'time': {'url': `${req.protocol}://${req.host}/${req.baseUrl}/time`}
+  });
+});
+
+export default router;
