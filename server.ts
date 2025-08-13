7d57b5d5650ac1378d07cd79f46793c1723625d8
Lladnaar <mark.randall@email.com>
2025-08-13 15:41:07 +1200

    Relocated URIs

--- server.ts
+++ server.ts
@@ -14,16 +14,16 @@ server.use(express.json());
 
 // HATEOAS discovery
 
-server.get('/', (req: express.Request, res: express.Response) => {
+server.get('/api', (req: express.Request, res: express.Response) => {
   res.send({
-    'time': {'url': `${req.protocol}://${req.host}/time`}
+    'time': {'url': `${req.protocol}://${req.host}/api/time`}
   });
 });
 
 // Resource definitions
 
-server.use('/app', express.static(settings.appfiles));
-server.use('/time', timeRouter);
+server.use('/', express.static(settings.appfiles));
+server.use('/api/time', timeRouter);
 
 server.all('/{*any}', (req: express.Request, res: express.Response, next: express.NextFunction) => {
   res.status(404).send('Resource not found');
