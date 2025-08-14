845fae3cd71eec565dee3ecfb409acdb1cd995b7
Lladnaar <mark.randall@email.com>
2025-08-14 17:45:30 +1200

    Memory user set

--- user.ts
+++ user.ts
@@ -3,19 +3,26 @@
 import express from 'express';
 
 const router = express.Router();
+var user = new Set();
 
 // Resource and verb definitions 
 
 router.get('/:userid', (req: express.Request, res: express.Response) => {
     console.debug(`User ${req.params.userid} requested`);
-    res.send({
-      'url': `${req.protocol}://${req.host}${req.baseUrl}/${req.params.userid}`,
-      'userid': req.params.userid
-    });
+    if (user.has(req.params.userid)) {
+        res.send({
+            'url': `${req.protocol}://${req.host}${req.baseUrl}/${req.params.userid}`,
+            'userid': req.params.userid
+        });
+    }
+    else {
+        res.status(404).send();
+    }
 });
 
 router.put('/:userid', (req: express.Request, res: express.Response) => {
     console.debug(`User ${req.params.userid} updated`);
+    user.add(req.params.userid);
     res.status(201).send({
       'url': `${req.protocol}://${req.host}${req.baseUrl}/${req.params.userid}`,
       'userid': req.params.userid
@@ -24,6 +31,7 @@ router.put('/:userid', (req: express.Request, res: express.Response) => {
 
 router.delete('/:userid', (req: express.Request, res: express.Response) => {
     console.debug(`User ${req.params.userid} deleted`);
+    user.delete(req.params.userid);
     res.status(204).send();
 });
 
