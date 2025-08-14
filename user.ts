eb03c4c6e720aa3206482f01f2de43c1bbc6ffbc
Lladnaar <mark.randall@email.com>
2025-08-14 17:40:17 +1200

    Fake user put and delete

--- user.ts
+++ user.ts
@@ -7,11 +7,24 @@ const router = express.Router();
 // Resource and verb definitions 
 
 router.get('/:userid', (req: express.Request, res: express.Response) => {
-    console.debug('User ${req.params.userid} requested');
+    console.debug(`User ${req.params.userid} requested`);
     res.send({
       'url': `${req.protocol}://${req.host}${req.baseUrl}/${req.params.userid}`,
       'userid': req.params.userid
     });
+});
+
+router.put('/:userid', (req: express.Request, res: express.Response) => {
+    console.debug(`User ${req.params.userid} updated`);
+    res.status(201).send({
+      'url': `${req.protocol}://${req.host}${req.baseUrl}/${req.params.userid}`,
+      'userid': req.params.userid
+    });
+});
+
+router.delete('/:userid', (req: express.Request, res: express.Response) => {
+    console.debug(`User ${req.params.userid} deleted`);
+    res.status(204).send();
 });
 
 export default router;
