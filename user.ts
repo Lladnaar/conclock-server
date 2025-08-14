33677c2932c85f5a9ebecea333db81d9f28981f3
Lladnaar <mark.randall@email.com>
2025-08-14 18:05:37 +1200

    First attempt at redis - broken

--- user.ts
+++ user.ts
@@ -1,38 +1,47 @@
 // user resource definition
 
 import express from 'express';
+import { createClient } from 'redis';
 
 const router = express.Router();
-var user = new Set();
+
+const client = createClient();
+client.on('error', err => console.log('Redis Client Error', err));
+await client.connect();
 
 // Resource and verb definitions 
 
 router.get('/:userid', (req: express.Request, res: express.Response) => {
     console.debug(`User ${req.params.userid} requested`);
-    if (user.has(req.params.userid)) {
-        res.send({
-            'url': `${req.protocol}://${req.host}${req.baseUrl}/${req.params.userid}`,
-            'userid': req.params.userid
-        });
-    }
-    else {
-        res.status(404).send();
-    }
+    var exists = client.exists(`user:{req.params.userid}`).then(() => {
+        if (exists === 1) {
+            res.send({
+                'url': `${req.protocol}://${req.host}${req.baseUrl}/${req.params.userid}`,
+                'userid': req.params.userid,
+                'value': value
+            });
+        }
+        else {
+            res.status(404).send();
+        }
+    })
 });
 
 router.put('/:userid', (req: express.Request, res: express.Response) => {
     console.debug(`User ${req.params.userid} updated`);
-    user.add(req.params.userid);
-    res.status(201).send({
-      'url': `${req.protocol}://${req.host}${req.baseUrl}/${req.params.userid}`,
-      'userid': req.params.userid
+    client.set(`user:{req.params.userid}`,'true').then(() => {
+        res.status(201).send({
+        'url': `${req.protocol}://${req.host}${req.baseUrl}/${req.params.userid}`,
+        'userid': req.params.userid
+        });
     });
 });
 
 router.delete('/:userid', (req: express.Request, res: express.Response) => {
     console.debug(`User ${req.params.userid} deleted`);
-    user.delete(req.params.userid);
-    res.status(204).send();
+    client.del(`user:{req.params.userid}`).then(() => {
+        res.status(204).send();
+    })
 });
 
 export default router;
