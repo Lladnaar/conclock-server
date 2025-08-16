93d6c5d84eb9415aa4bebd8338bc9343e8f3e27a
Lladnaar <mark.randall@email.com>
2025-08-16 14:58:31 +1200

    Moved logging to be accurate

--- user.ts
+++ user.ts
@@ -13,16 +13,17 @@ console.info('Connected to Redis data server');
 // Resource and verb definitions 
 
 router.get('/:userid', (req: express.Request, res: express.Response) => {
-    console.debug(`User ${req.params.userid} requested`);
     client.exists(`user:${req.params.userid}`).then((exists) => {
         if (exists === 1) {
             res.send({
                 'url': `${req.protocol}://${req.host}${req.baseUrl}/${req.params.userid}`,
                 'userid': req.params.userid
             });
+            console.debug(`User ${req.params.userid} retrieved`);
         }
         else {
             res.status(404).send();
+            console.debug(`User ${req.params.userid} not found`);
         }
     }).catch(() => {
         console.error('Unexpeceted User fetch error');
@@ -31,12 +32,12 @@ router.get('/:userid', (req: express.Request, res: express.Response) => {
 });
 
 router.put('/:userid', (req: express.Request, res: express.Response) => {
-    console.debug(`User ${req.params.userid} updated`);
     client.set(`user:${req.params.userid}`,'true').then(() => {
         res.status(201).send({
-        'url': `${req.protocol}://${req.host}${req.baseUrl}/${req.params.userid}`,
-        'userid': req.params.userid
+            'url': `${req.protocol}://${req.host}${req.baseUrl}/${req.params.userid}`,
+            'userid': req.params.userid
         });
+        console.debug(`User ${req.params.userid} updated`);
     }).catch(() => {
         console.error('Unexpeceted User upsert error');
         res.status(500).send({'error':'Unexpected upsert error'});
