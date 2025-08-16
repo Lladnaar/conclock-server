64efe07b93f753dde7af3b0562716dfcae6b50ee
Lladnaar <mark.randall@email.com>
2025-08-16 17:17:12 +1200

    Moved basics of store/retrive to redis module

--- data_redis.ts
+++ data_redis.ts
@@ -1,8 +1,28 @@
 import { createClient } from 'redis';
-const client = createClient();
 
+export const client = createClient();
 client.on('error', err => console.error('Redis Client Error', err));
 await client.connect();
 console.info('Connected to Redis data server');
 
-export default client;
+export class User {
+    static list() {
+
+    }
+    
+    static exists(id: string) {
+        return client.exists(`user:${id}`);
+    }
+
+    static get(id: string) {
+
+    }
+
+    static set(id: string, content: object) {
+        return client.set(`user:${id}`, 'true');
+    }
+
+    static delete(id) {
+        return client.del(`user:${id}`);
+    }
+}
--- user.ts
+++ user.ts
@@ -1,14 +1,14 @@
 // user resource definition
 
 import express from 'express';
-import client from './data_redis.ts';
+import {client, User} from './data_redis.ts';
 
 const router = express.Router();
 
 // Resource and verb definitions 
 
 router.get('/:userid', (req: express.Request, res: express.Response) => {
-    client.exists(`user:${req.params.userid}`).then((exists) => {
+    User.exists(req.params.userid).then((exists) => {
         if (exists === 1) {
             res.send({
                 'url': `${req.protocol}://${req.host}${req.baseUrl}/${req.params.userid}`,
@@ -21,32 +21,32 @@ router.get('/:userid', (req: express.Request, res: express.Response) => {
             console.debug(`User ${req.params.userid} not found`);
         }
     }).catch(() => {
-        console.error('Unexpeceted User fetch error');
+        console.error('Unexpected User fetch error');
         res.status(500).send({'error':'Unexpeceted fetch error'});
     });
 });
 
 router.put('/:userid', (req: express.Request, res: express.Response) => {
-    client.set(`user:${req.params.userid}`,'true').then(() => {
+    User.set(req.params.userid, {'true':'true'}).then(() => {
         res.status(201).send({
             'url': `${req.protocol}://${req.host}${req.baseUrl}/${req.params.userid}`,
             'userid': req.params.userid
         });
         console.debug(`User ${req.params.userid} updated`);
     }).catch(() => {
-        console.error('Unexpeceted User upsert error');
+        console.error('Unexpected User upsert error');
         res.status(500).send({'error':'Unexpected upsert error'});
     });
 });
 
 router.delete('/:userid', async (req: express.Request, res: express.Response) => {
     try {
-        await client.del(`user:${req.params.userid}`);
+        await User.delete(req.params.userid);
         res.status(204).send();
         console.debug(`User ${req.params.userid} deleted`);
     }
     catch {
-        console.error('Unexpeceted User delete error');
+        console.error('Unexpected User delete error');
         res.status(500).send({'error':'Unexpected delete error'});
     }
 });
