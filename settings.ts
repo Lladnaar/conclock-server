3b3af1ed8a327ae38007466bfc53c9ac1dd50b41
Lladnaar <mark.randall@email.com>
2025-08-24 12:25:17 +1200

    Allow for local settings with defaults
    fix local

--- settings.ts
+++ settings.ts
@@ -1,4 +1,21 @@
-export default class settings {
-	static port = 9000;
-    static appfiles = 'app';
-}
+const settingsLocal = await import('./settings-local.ts')
+    .then(module => module.default)
+    .catch(() => ({})); // Fallback to empty object if local settings cannot be loaded
+
+const settingsDefault = {
+	port: 9000,
+    appfiles: 'app'
+};
+
+const settings = {...settingsDefault, ...settingsLocal};
+export default settings;
+
+/* To create local setting overrides, copy the below sample and save as settings-local.ts
+
+--- BEGIN COPY ---
+const settings = {
+    setting: "value"
+};
+
+export default settings;
+--- END COPY --- */
