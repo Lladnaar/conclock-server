b1b6bdd449fefa93a76667a6231e81e730c03dea
Lladnaar <mark.randall@email.com>
2025-08-24 12:25:17 +1200

    Allow for local settings with defaults

--- settings.ts
+++ settings.ts
@@ -1,4 +1,10 @@
-export default class settings {
-	static port = 9000;
-    static appfiles = 'app';
-}
+const settingsLocal = await import('./settings-local.ts')
+    .catch(() => ({})); // Fallback to empty object if local settings cannot be loaded
+
+const settingsDefault = {
+	port: 9000,
+    appfiles: 'app'
+};
+
+const settings = {...settingsDefault, ...settingsLocal};
+export default settings;
