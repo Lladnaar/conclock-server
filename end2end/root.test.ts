8bfa05b9c0a4752ee65b74ced277b562b74ec72c
Lladnaar <mark.randall@email.com>
2025-09-14 00:34:05 +1200

    End-to-end: Root

---
+++ end2end/root.test.ts
@@ -0,0 +1,15 @@
+import {describe, expect, test} from "vitest";
+import axios from "axios";
+
+const baseUrl = "http://localhost:8080/api";
+
+describe("Root", () => {
+    test("Core endpoints exist", async () => {
+        const response = await axios.get(baseUrl);
+
+        expect(response.status).toBe(200);
+        expect(response.data).toHaveProperty("time");
+        expect(response.data).toHaveProperty("user");
+        expect(response.data).toHaveProperty("event");
+    });
+});
